00db85309fa72a0be41dca0464f8372c490ba895
Sahil Garhwal <garhwalsahil007@gmail.com>
2018-10-07 02:38:15 +0530

    week 4, 1st commit

---
+++ R prog/week 4 assignment/best.R
@@ -0,0 +1,28 @@
+best<-function(state,outcome){
+    
+    data<-read.csv("outcome-of-care-measures.csv",colClasses = "character")
+    data1<-as.data.frame(cbind(data[,2], 
+                               data[,7],
+                               data[,11],
+                               data[,17],
+                               data[,23]),
+                         stringsAsFactors = FALSE)
+    colnames(data1)<-c("hospital", "Hstate", "heart attack", "heart failure",
+                       "pneumonia")
+    if(!state %in% data1[,"Hstate"]){
+        stop("invalid state")
+    }
+    else if(!outcome %in% c("heart attack", "heart failure", "pneumonia")){
+        stop("invalid state")
+    }
+    else{
+       
+       min_val<- tapply(as.numeric(data1[,outcome]),data1[,"Hstate"],function(x) 
+                                                                            min(x,na.rm = TRUE),simplify = FALSE)
+       
+       result<-data1[,"hospital"][which(as.numeric(data1[,outcome])==min_val$state)]
+        
+    }
+    
+         
+}
