a57dde6d24d9ac9389748abf133041aaa5b52dd0
Sahil Garhwal <garhwalsahil007@gmail.com>
2018-10-09 23:21:52 +0530

    10th commit

--- R prog/week 4 assignment/rankall.R
+++ R prog/week 4 assignment/rankall.R
@@ -17,36 +17,49 @@ rankall<-function(outcome,num){
     data2<-with(data1,split(data1,Hstate))
     if(is.numeric(num)){
         for(i in seq_along(data2)){
-            data2[[i]]<-data2[[i]][order(suppressWarnings(as.numeric(data2[[i]][,outcome])),data2[[i]][,"hospital"]),]
+            data2[[i]]<-data2[[i]][order
+                                   (suppressWarnings(
+                                       as.numeric(data2[[i]][,outcome])),
+                                       data2[[i]][,"hospital"]),]
             ordered[[i]]<-c(data2[[i]][num,"hospital"],data2[[i]][1,"Hstate"])
         
         
         }
         result <- do.call(rbind, ordered)
-        output <- as.data.frame(result, row.names = result[, 2], stringsAsFactors = FALSE)
+        output <- as.data.frame(result, row.names = result[, 2], 
+                                stringsAsFactors = FALSE)
         names(output) <- c("hospital", "state")
     }
     if(num=="best"){
         for(i in seq_along(data2)){
-            data2[[i]]<-data2[[i]][order(suppressWarnings(as.numeric(data2[[i]][,outcome])),data2[[i]][,"hospital"]),]
+            data2[[i]]<-data2[[i]][order
+                                   (suppressWarnings(
+                                       as.numeric(data2[[i]][,outcome])),
+                                       data2[[i]][,"hospital"]),]
             ordered[[i]]<-c(data2[[i]][1,"hospital"],data2[[i]][1,"Hstate"])
             
             
         }
         result <- do.call(rbind, ordered)
-        output <- as.data.frame(result, row.names = result[, 2], stringsAsFactors = FALSE)
+        output <- as.data.frame(result, row.names = result[, 2]
+                                , stringsAsFactors = FALSE)
         names(output) <- c("hospital", "state")
         
     }
     if(num=="worst"){
         for(i in seq_along(data2)){
-            data2[[i]]<-data2[[i]][order(suppressWarnings(as.numeric(data2[[i]][,outcome])),data2[[i]][,"hospital"],decreasing = TRUE),]
+            data2[[i]]<-data2[[i]][order
+                                   (suppressWarnings(
+                                       as.numeric(data2[[i]][,outcome])),
+                                       data2[[i]][,"hospital"],
+                                       decreasing = TRUE),]
             ordered[[i]]<-c(data2[[i]][1,"hospital"],data2[[i]][1,"Hstate"])
             
             
         }
         result <- do.call(rbind, ordered)
-        output <- as.data.frame(result, row.names = result[, 2], stringsAsFactors = FALSE)
+        output <- as.data.frame(result, row.names = result[, 2], 
+                                stringsAsFactors = FALSE)
         names(output) <- c("hospital", "state")
         
     }
